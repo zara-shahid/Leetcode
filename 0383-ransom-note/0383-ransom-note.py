3b868b9a402a2f7f2af3466670b9d9ee1e73993f
Zara Shahid <zarashahid444@gmail.com>
2025-09-05 18:24:49 +0500

    Time: 3 ms (97.92%), Space: 18 MB (35.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,16 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ans={}
-        count=0
-        for i in range(0,len(magazine)):
-            if magazine[i] in ans:
-                ans[magazine[i]]+=1
-            else:
-                ans[magazine[i]]=1
-        for ch in range(0,len(ransomNote)):
-            if ransomNote[ch] in ans and ans[ransomNote[ch]]>0:
-                ans[ransomNote[ch]]-=1
-            else:
+        for i in set(ransomNote):
+            if magazine.count(i) < ransomNote.count(i):
                 return False
         return True
 
