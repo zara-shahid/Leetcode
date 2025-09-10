b66ea47647798630a981554493865a8125f3e40e
Zara Shahid <zarashahid444@gmail.com>
2025-09-10 16:04:50 +0500

    Time: 9 ms (68.34%), Space: 19.5 MB (24.47%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,8 +1,5 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n=len(nums)
-        # duplicate = 
-        # missing = 
-        # return [duplicate,missing]
         return [(sum(nums) - sum(set(nums))),(n*(n+1)//2) - sum(set(nums))]
         
