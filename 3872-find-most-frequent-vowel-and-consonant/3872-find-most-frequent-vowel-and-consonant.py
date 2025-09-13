62da05ffa3578a1b61fbd48b33db7f456cdaa344
Zara Shahid <zarashahid444@gmail.com>
2025-09-13 20:28:18 +0500

    Time: 7 ms (15.17%), Space: 18 MB (20.04%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -2,8 +2,5 @@ from collections import Counter
 
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        vowels = "aeiou"
-        c_vowel = Counter(ch for ch in s if ch in vowels)
-        c_conso = Counter(ch for ch in s if ch.isalpha() and ch not in vowels)
-        return (max(c_vowel.values(), default=0) +
-                max(c_conso.values(), default=0))
+        return (max(Counter(ch for ch in s if ch in "aeiou").values(), default=0) +
+                max(Counter(ch for ch in s if ch.isalpha() and ch not in "aeiou").values(), default=0))
