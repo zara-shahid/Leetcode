778ce98b0f6c7f0ade09d1fb0ebf09df9dec965c
Zara Shahid <zarashahid444@gmail.com>
2025-11-09 14:59:47 +0500

    Time: 35 ms (74.59%), Space: 17.9 MB (28.06%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches=0
+        teams=n
+        while teams>1:
+            if teams%2!=0:
+                matches+=(teams-1)//2
+                teams=((teams-1)//2)+1
+                
+                
+            else:
+                matches+=teams//2
+                teams=teams//2
+                
+        return matches
+
+
+        
