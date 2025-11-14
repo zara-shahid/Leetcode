2bbcc2be39f03bd62f4b485cc997a699e0003b27
Zara Shahid <zarashahid444@gmail.com>
2025-11-14 21:28:57 +0500

    Time: 0 ms (100%), Space: 17.7 MB (89.14%) - LeetHub

---
+++ 1965-sum-of-digits-in-base-k/1965-sum-of-digits-in-base-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        ans=0
+        while n>0:
+            rem=n%k
+            n=n//k
+            ans+=rem
+        n+=1
+        return ans
+
+
+        
