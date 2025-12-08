e89666581e299faf466d5bc020e9b071ab2fa3f6
Zara Shahid <zarashahid444@gmail.com>
2025-12-08 20:18:16 +0500

    Time: 0 ms (100%), Space: 17.7 MB (58.55%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if k<=numOnes:
+            return k
+        if k<=numOnes+numZeros:
+            return numOnes
+        if k>numOnes+numZeros:
+            ans=k-(numOnes+numZeros)
+            return numOnes-ans
+        
+        
