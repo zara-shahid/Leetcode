cded6f0c33380133f1cfb70700b5c21bf7b6ddd7
Zara Shahid <zarashahid444@gmail.com>
2026-01-15 14:04:19 +0500

    Time: 39 ms (92.97%), Space: 20.2 MB (23.89%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,38 +1,18 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []      
         ans = 0
-        current_no = 0
-        operator = 1     
-        i = 0
-        n = len(s)
-        
-        while i < n:
-            char = s[i]
-            
-            if char.isdigit():
-                current_no = 0
-                while i < n and s[i].isdigit():
-                    current_no = current_no * 10 + int(s[i])
-                    i += 1
-                ans += operator * current_no
-                continue 
-            
-            elif char == '+':
-                operator = 1
-            
-            elif char == '-':
-                operator = -1
-            
-            elif char == '(':
-                stack.append((ans, operator))
-                ans = 0
-                operator = 1  
-            
-            elif char == ')':
-                prev_ans, prev_operator = stack.pop()
-                ans = prev_ans + prev_operator * ans
-            
-            i += 1
-        
-        return ans
+        num = 0
+        sign = 1
+        stack = [sign] 
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c == '(':
+                stack.append(sign)
+            elif c == ')':
+                stack.pop()
+            elif c == '+' or c == '-':
+                ans += sign * num
+                sign = (1 if c == '+' else -1) * stack[-1]
+                num = 0
+        return ans + sign * num
