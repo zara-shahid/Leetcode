961464ef4859c170f550841bdeadc442f102bf41
Zara Shahid <zarashahid444@gmail.com>
2026-03-04 20:17:34 +0500

    Time: 0 ms (100%), Space: 19.3 MB (58.44%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -0,0 +1,10 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        image = [row[::-1] for row in image] 
+        for i in range(len(image)):
+            for j in range(len(image)):
+                if image[i][j]==0:
+                    image[i][j]=1
+                else:
+                    image[i][j]=0
+        return image
