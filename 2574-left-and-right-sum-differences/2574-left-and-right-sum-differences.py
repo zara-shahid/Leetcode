5c47bbc17d0cd79167af531ac3c8c0155557a068
Zara Shahid <zarashahid444@gmail.com>
2026-09-07 17:05:03 +0500

    Time: 3 ms (70.54%), Space: 19.5 MB (24.5%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -4,10 +4,12 @@ class Solution:
         leftSum = 0
         rightSum = 0
         total = sum(nums)
+
         for i in range(len(nums)):
             rightSum = total - leftSum - nums[i]
             ans.append(abs(leftSum - rightSum))
             leftSum+=nums[i]
+            
         return ans
         
 
